881ade168530f18ce25d672f8425c429fa8daa56
gimgyumin <qmean59@gmail.com>
2024-11-03 20:44:30 +0900

    initialize repository

---
+++ src/main/java/javalab/umc7th_mission/Umc7thMissionApplication.java
@@ -0,0 +1,13 @@
+package javalab.umc7th_mission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc7thMissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Umc7thMissionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/javalab/umc7th_mission/Umc7thMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package javalab.umc7th_mission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umc7thMissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
